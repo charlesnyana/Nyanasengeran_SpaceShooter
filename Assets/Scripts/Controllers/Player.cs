d51201ace78d7149c7b8c214c1a4250acc9050c8
charlesnyana <150265059+charlesnyana@users.noreply.github.com>
2024-09-17 14:11:51 -0400

    Exercise 1: Player Movement finished. Started with edge detection logic.

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -9,9 +9,40 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    Vector3 relativePos;
+
+    public float speed;
+
     void Update()
     {
-
+        
     }
 
+    void FixedUpdate()
+    {
+        relativePos = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            PlayerMove(Vector3.left);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            PlayerMove(Vector3.right);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            PlayerMove(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            PlayerMove(Vector3.down);
+        }
+    }
+
+    void PlayerMove(Vector3 offset)
+    {
+        if (relativePos.x < 0) speed = 0;
+        transform.position = transform.position + (offset * speed * Time.deltaTime);
+    }
 }
