68ff7cdf5539b4f2bb3791b1d8e42bbaf4570e45
Charles Nyanasengeran <nyanasen@shernet.sheridancollege.ca>
2024-09-24 14:39:22 -0400

    Classes in Week 4 degrees stuff.

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -12,10 +13,43 @@ public class Player : MonoBehaviour
     Vector3 relativePos;
 
     public float speed;
+    public float baseSpeed;
+    public float maxSpeed;
+
+    bool accelerating = false;
+    public float accelRate;
+    public float deccelRate;
+
+    public List<int> angles;
+    int angleIndex = 0;
+    Vector3 endPoint;
+
+    private void Start()
+    {
+        baseSpeed = speed;
+    }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float rad = angles[angleIndex] * Mathf.Deg2Rad;
+
+            float xPos = Mathf.Cos(rad); //x
+            float yPos = Mathf.Sin(rad); //y
+
+            endPoint = new Vector3(xPos, yPos, 0);
+
+            Debug.Log("drawing angle: " + angles[angleIndex] + " with endpoint at: " + endPoint);
+
+            if (angleIndex == 10) angleIndex = 0;
+            angleIndex++;
+        }
+        Debug.DrawLine(Vector3.zero, endPoint, Color.red);
+
         
+
+
     }
 
     void FixedUpdate()
@@ -24,24 +58,56 @@ public class Player : MonoBehaviour
 
         if (Input.GetKey(KeyCode.LeftArrow) && relativePos.x > 0)
         {
-            PlayerMove(Vector3.left);
+            PlayerMovement(Vector3.left);
+            accelerating = true;
         }
         if (Input.GetKey(KeyCode.RightArrow) && relativePos.x < 1)
         {
-            PlayerMove(Vector3.right);
+            PlayerMovement(Vector3.right);
+            accelerating = true;
         }
         if (Input.GetKey(KeyCode.UpArrow) && relativePos.y < 1)
         {
-            PlayerMove(Vector3.up);
+            PlayerMovement(Vector3.up);
+            accelerating = true;
         }
         if (Input.GetKey(KeyCode.DownArrow) && relativePos.y > 0)
         {
-            PlayerMove(Vector3.down);
+            PlayerMovement(Vector3.down);
+            accelerating = true;
+        }
+
+        if (accelerating == false)
+        {
+            accelCalc();
         }
     }
 
-    void PlayerMove(Vector3 offset)
+    void PlayerMovement(Vector3 offset)
     {
+        accelerating = true;
+        accelCalc();
+
+        if (speed >= maxSpeed)
+        {
+            Debug.Log("slow down");
+            accelerating = false;
+        }
+
         transform.position += offset * speed * Time.deltaTime;
+    }
+
+    void accelCalc()
+    {
+        if (accelerating == false && speed >= baseSpeed)
+        {
+            speed -= deccelRate * Time.deltaTime;
+        } else if (speed < maxSpeed)
+        {
+            accelerating = true;
+            speed += accelRate * Time.deltaTime;
+        }
+
+        speed = Mathf.Clamp(speed, baseSpeed, maxSpeed);
     }
 }
